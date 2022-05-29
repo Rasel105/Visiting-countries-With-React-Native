beeed0cdb1c0214541804ef44c561a01e31a603e
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-29 12:23:49 +0600

    project created and make a components

---
+++ components/Countries.js
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+
+
+export default function Countries() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://restcountries.com/v3.1/all`)
+            .then(res => res.json())
+            .then(data => {
+                setCountries(data);
+                console.log(data);
+            })
+    }, [])
+
+    return (
+        <View>
+            <Text>Countries: {countries.length}</Text>
+        </View>
+    )
+}
